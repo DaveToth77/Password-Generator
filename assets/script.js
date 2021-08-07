8baa4c5515155082a6c854c960b409570ed64bf0
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-08-07 14:11:10 -0400

    added password component prompts

--- assets/script.js
+++ assets/script.js
@@ -9,3 +9,13 @@ function generatePassword() {
     while (passwordLength < 8 || passwordLength > 128 || passwordLength == null) {
         passwordLength = prompt('Please pick a number between 8 and 128');
     }
+      // password components prompts
+  let upper = confirm("Do you want to add uppercase characters? ");
+  let lower = confirm("Do you want to add lowercase characters? ");
+  let number = confirm("Do you want to add number characters? ");
+  let special = confirm("Do you want to add special characters? ");
+
+  if (upper != true && lower != true && number != true && special != true) {
+      alert("You must select at least one character type!")
+  }
+
